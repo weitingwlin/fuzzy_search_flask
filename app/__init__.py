ebee80dd8b3b1c46374ff06061f90b6c23b0d534
weitingwlin <weiting.lin.lin@gmail.com>
2018-04-20 08:38:08 -0700

    remove flask_bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,6 @@ from flask import Flask, session
 from flask.ext.session import Session
 from config import Config
 
-# from flask_bootstrap import Bootstrap
-
 app = Flask(__name__)
 # app.config.from_object(Config)
 sess = Session()
